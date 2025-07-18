639c8e4b3279fbf6761912205ced2ecee0399a7e
gubrey <122058498+gubrey@users.noreply.github.com>
2025-07-18 04:28:50 -0500

    I used AI to write this, welcome to the future
    Introduces the stoopidfatguy NPC with walking animation and movement logic to map/start/sockzone. Updates sock zone assets and references, adds new textures (ant.png, wall.jpg, big fat lard.png), and refactors resource paths for consistency. Also improves temmie_flakes_reminds_me_of_jeffy.gd to use a marker for player teleportation.

--- map/start/sockzone/temmie_flakes_reminds_me_of_jeffy.gd
+++ map/start/sockzone/temmie_flakes_reminds_me_of_jeffy.gd
@@ -3,7 +3,8 @@ extends Area2D
 # cinder cinder cinder i made you out of clay! cinder cinder cinder with cinder i will play!
 #btw thats my cinder remix of dreidel song by jew
 #for my next song, i will turn water into funk!
+@onready var marker_2d = $Marker2D
 
 func _process(_delta):
 	if Input.is_action_just_pressed("player_interact") and overlaps_body(Game.player):
-		Game.player.position = Vector2()
+		Game.player.position = marker_2d.global_position
