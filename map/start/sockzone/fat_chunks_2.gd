dd5336985e25cb6b94978aa62f1c883a1e8a7339
Razz <89815386+SirAxolot@users.noreply.github.com>
2025-07-20 23:33:23 -0400

    razby gambling
    razby gambling

--- map/start/sockzone/fat_chunks_2.gd
+++ map/start/sockzone/fat_chunks_2.gd
@@ -1,6 +1,13 @@
 extends Sprite2D
 
-func _process(_delta: float) -> void:
+var time = 0.0
+@onready var talking_speech_bubble: TalkingSpeechBubble = $TalkingSpeechBubble
+func _process(delta: float) -> void:
+	time += delta * (4 if talking_speech_bubble.talking else 1)
 	scale = Vector2.ONE
-	scale += Vector2(sin(Time.get_ticks_msec() / 300.), -sin(Time.get_ticks_msec() / 300.)) * 0.1
+	scale += Vector2(sin(time * 3), -sin(time * 3)) * 0.05
 	scale *= 0.75
+
+func _on_talk_hitbox_body_entered(body: Node2D) -> void:
+	if body == Game.player && !talking_speech_bubble.talking:
+		talking_speech_bubble.say("im ganbling!!", 1.0 / 15.0, 2)
