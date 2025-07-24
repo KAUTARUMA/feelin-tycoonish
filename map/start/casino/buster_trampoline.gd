59f13ed49812ab97a730c4b241547859caa0a627
Falkimore <86729768+Falkimore@users.noreply.github.com>
2025-07-24 18:43:06 -0500

    less realistic bounce

--- map/start/casino/buster_trampoline.gd
+++ map/start/casino/buster_trampoline.gd
@@ -14,7 +14,6 @@ func _on_area_2d_body_entered(body):
 	if body.velocity.y < 0 :
 		return
 	
-	body.global_position.y = global_position.y - 20
 	body.velocity.y = UPWARDS_VELOCITY
 	
 	audio_player.stop()
